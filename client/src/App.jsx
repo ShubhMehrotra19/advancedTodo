6d613bad4b79f9d9f6a65a2f7ed975af4fb54049
unknown <hishubh09@gmail.com>
2025-07-29 15:33:32 +0530

    feat : create card holder component and card components for both modes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
+import CardHolder from './components/task-window/CardHolder';
 
 const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -42,8 +43,15 @@ const App = () => {
   return (
     <>
       <Navbar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-      <main className="relative pt-16 bg-gray-100 dark:bg-gray-950 min-h-screen transition-colors ease-in-out duration-300 flex items-center justify-center">
-        <p className='select-none fixed text-[130px] font-bold text-stone-400/40 dark:text-gray-800/40 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-colors ease-in-out duration-300'>TODO</p>
+      <main className="relative pt-16 bg-stone-50/60 dark:bg-gray-950 min-h-screen transition-colors ease-in-out duration-300 flex items-start justify-center">
+        <p className='select-none fixed text-[130px] font-bold text-stone-300/40 dark:text-gray-800/40 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-colors ease-in-out duration-300'>TODO</p>
+        
+        {/*  this section will be holding the main content of the app */}
+        <section className='fixed w-full flex justify-evenly items-start px-5 mt-8'>
+          <CardHolder color={`#EAB308`} progressBar={"To Do"} />
+          <CardHolder color={"#3B82F6"} progressBar={"In Progress"} />
+          <CardHolder color={`#22C55E`} progressBar={"Completed"} />
+        </section>
       </main>
     </>
   );
